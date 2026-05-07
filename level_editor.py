cafe31ed22ce965aa69c70d59f6c27079875d9ec
keno555552 <k024g1024@g.neec.ac.jp>
2026-05-07 13:03:41 +0900

    TL_01_06v1

--- level_editor.py
+++ level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 # ブレンダーに登録するアドオン情報
 bl_info = {
@@ -42,6 +43,7 @@ def unregister():
         bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました。")
 
+### ============================ オブジェクト生成 ============================ ###
 
 #オペレータ 頂点を伸ばす
 class MYADDON_OT_stretch_vertex(bpy.types.Operator):
@@ -74,6 +76,46 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
 
         #オペレータの命令終了を通知
         return {'FINISHED'}
+    
+### ============================== システム関連 =============================== ###
+
+#オペレータ シーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+
+    def execute(self, context):
+
+        print("シーン情報をExportします")
+        
+        #シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+            #ローカルトランスフォーム行列から平行移動、回転、スケーリングを抽出
+            #型は Vector, Quaternion, Vector
+            trans, rot, scale = object.matrix_local.decompose()
+            #回転を Quaternion から Euler （3軸での回転角）に変換
+            rot = rot.to_euler()
+            #ラジアンから度数法に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+            #トランスフォーム情報を表示
+            print("Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z) )
+            print("Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z) )
+            print("Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z) )
+            #親オブジェクトの名前を表示
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            print()
+
+
+        print("シーン情報をExportしました")
+        self.report({'INFO'}, "シーン情報をExportしました")
+
+        return {'FINISHED'}
+
 
 #トップバーの拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
@@ -87,6 +129,10 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     # サブメニューの描画
     def draw(self, context):
         #トップバーの「エディターメニュー」に項目（オペレータ）を追加
+
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname, text=MYADDON_OT_export_scene.bl_label)
+
+        self.layout.separator() # 区切り線を追加
         self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text=MYADDON_OT_stretch_vertex.bl_label)
         self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text=MYADDON_OT_create_ico_sphere.bl_label)
 
@@ -104,7 +150,10 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
 
 #Blenderに登録するクラスリスト
 classes = (
+    TOPBAR_MT_my_menu,
+    ### ============================ ###
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_create_ico_sphere,
-    TOPBAR_MT_my_menu
+    ### ============================ ###
+    MYADDON_OT_export_scene
 )
