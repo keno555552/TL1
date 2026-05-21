1701e6cd367ce20bfa4f722a22551d9329f91e77
keno555552 <k024g1024@g.neec.ac.jp>
2026-05-21 13:44:39 +0900

    TL_01_11v1

--- level_editor.py
+++ level_editor.py
@@ -2,6 +2,7 @@ from multiprocessing import context
 
 import copy
 import math
+import json
 from textwrap import indent
 
 import bpy
@@ -67,7 +68,7 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
     bl_description = "シーン情報をExportします"
 
     #出力ファイルの拡張子
-    filename_ext = ".scene"
+    filename_ext = ".json"
 
     def write_and_print(self, file, str):
 
@@ -106,7 +107,7 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
             temp_str = indent + "CC %f %f %f"
             temp_str = temp_str % (object["collider_center"][0], object["collider_center"][1], object["collider_center"][2])
             self.write_and_print(file, temp_str)
-            
+
             temp_str = indent + "CS %f %f %f"
             temp_str = temp_str % (object["collider_size"][0], object["collider_size"][1], object["collider_size"][2])
             self.write_and_print(file, temp_str)
@@ -116,6 +117,59 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
 
         for child in object.children:
             self.parse_scene_recursive(file, child, level + 1)
+
+
+    def parse_scene_recursive_json(self, data_parent, object, level):
+        # シーンのオブジェクト1個分のjsonオブジェクト生成
+            json_object = dict()
+            # オブジェクト種類
+            json_object["type"] = object.type
+            # オブジェクト名
+            json_object["name"] = object.name
+
+            # オブジェクトのローカルトランスフォームから
+            # 平行移動、回転、スケールを抽出
+            trans, rot, scale = object.matrix_local.decompose()
+            # 回転を Quaternion から Euler （3軸での回転角）に変換
+            rot = rot.to_euler()
+            # ラジアンから度数法に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+            # トランスフォーム情報をディクショナリに登録
+            transform = dict()
+            transform["translation"] = (trans.x, trans.y, trans.z)
+            transform["rotation"] = (rot.x, rot.y, rot.z)
+            transform["scaling"] = (scale.x, scale.y, scale.z)
+            # まとめて1個分のjsonオブジェクトに登録
+            json_object["transform"] = transform
+
+            # カスタムプロパティ 'file_name'
+            if "file_name" in object:
+                json_object["file_name"] = object["file_name"]
+            
+            # カスタムプロパティ 'collider'
+            if "collider" in object:
+                collider = dict()
+                collider["type"] = object["collider"]
+                collider["center"] = object["collider_center"].to_list()
+                collider["size"] = object["collider_size"].to_list()
+                json_object["collider"] = collider
+            
+
+
+            # 1個分のjsonオブジェクトを親オブジェクトに登録
+            data_parent.append(json_object)
+
+            # 子ノードがあれば
+            if len(object.children) > 0:
+                # 子ノードリストを作成
+                json_object["children"] = list()
+
+                # 子ノードへ進む（深さが1上がる）
+                for child in object.children:
+                    self.parse_scene_recursive_json(json_object["children"], child, level + 1)
+
 
 
     def export(self):
@@ -143,11 +197,49 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
 
         print("シーン情報をExportしました")
 
+    def export_json(self):
+        """JSON形式でファイルに出力"""
+
+        # 保存する情報をまとめるdict
+        json_object_root = dict()
+
+        # ノード名
+        json_object_root["name"] = "scene"
+        # オブジェクトリストを作成
+        json_object_root["objects"] = list()
+
+
+        #シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+
+            # 親オブジェクトがある場合はスキップ（親オブジェクトから再帰的に子オブジェクトも出力されるため）
+            if (object.parent):
+                continue
+            
+            # シーン直下のオブジェクトをルートノード(深さ0)とし、再帰関数で走査
+            self.parse_scene_recursive_json(json_object_root["objects"], object, 0)
+        
+
+        #オブジェクトをJson文字にエンコード(改行,インデントあり)
+        json_text = json.dumps(json_object_root, ensure_ascii=False, cls=json.JSONEncoder, indent=4)
+
+        # コンソールに表示してみる
+        print(json_text)
+
+        # ファイルをテキスト形式で書き出し用にオープン
+        # スコープを抜けると自動的にクローズされる
+        with open(self.filepath, "wt", encoding="utf-8") as file:
+        
+            # ファイルに文字列を書き込む
+            file.write(json_text)
+
+
+
     def execute(self, context):
 
         print("シーン情報をExportします")
 
-        self.export()
+        self.export_json()
         
         
         self.report({'INFO'}, "シーン情報をExportしました")
